1eac3128803ba17bd19e0b2019f1bce5365c3709
cashhishh <158817754+cashhishh@users.noreply.github.com>
2024-07-17 22:32:54 +0530

    Add files via upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,7 @@
+//var generateName = require("sillyname");
+// import generateName from "superhero";
+// var superhero= generateName();
+// console.log(`My name is ${superhero}.`);
+import superheroes from "superheroes";
+const name = superheroes.random();
+console.log(`I am ${name}.`);
